bea5eef1deb3a2e20d089e73de1873255539633b
John Schoeman <johnschoeman1617@gmail.com>
2017-08-06 21:25:05 -0700

    Complete Tic Tac Toe AI

--- W1D5/tictactoeai/skeleton/lib/super_computer_player.rb
+++ W1D5/tictactoeai/skeleton/lib/super_computer_player.rb
@@ -2,6 +2,15 @@ require_relative 'tic_tac_toe_node'
 
 class SuperComputerPlayer < ComputerPlayer
   def move(game, mark)
+    node = TicTacToeNode.new(game.board, mark)
+    children = node.children
+    children.each do |child|
+      return child.prev_move_pos if child.winning_node?(mark)
+    end
+    children.each do |child|
+      return child.prev_move_pos unless child.losing_node?(mark)
+    end
+    raise "I quit, this game is dumb."
   end
 end
 
--- W1D5/tictactoeai/skeleton/lib/tic_tac_toe_node.rb
+++ W1D5/tictactoeai/skeleton/lib/tic_tac_toe_node.rb
@@ -1,6 +1,9 @@
+require 'byebug'
 require_relative 'tic_tac_toe'
 
 class TicTacToeNode
+  attr_reader :board, :next_mover_mark, :prev_move_pos
+
   def initialize(board, next_mover_mark, prev_move_pos = nil)
     @board = board
     @next_mover_mark = next_mover_mark
@@ -8,31 +11,50 @@ class TicTacToeNode
   end
 
   def losing_node?(evaluator)
-
+    # byebug
+    if @board.over?
+      return true if @board.winner != evaluator && !@board.winner.nil?
+      return false if @board.winner == evaluator || @board.winner.nil?
+    else
+      if evaluator == @next_mover_mark
+        children.all? { |child| child.losing_node?(evaluator) }
+      elsif evaluator == get_next_mover(@next_mover_mark)
+        children.any? { |child| child.losing_node?(evaluator) }
+      end
+    end
   end
 
   def winning_node?(evaluator)
+    if @board.over?
+      return true if @board.winner == evaluator
+      return false if @board.winner != evaluator || @board.winner.nil?
+    else
+      if evaluator == @next_mover_mark
+        children.any? { |child| child.winning_node?(evaluator) }
+      elsif evaluator == get_next_mover(@next_mover_mark)
+        children.all? { |child| child.winning_node?(evaluator) }
+      end
+    end
   end
 
-  # This method generates an array of all moves that can be made after
-  # the current move.
   def children
     children = []
     empty_positions = all_empty_positions
+
     empty_positions.each do |pos|
       new_board = @board.dup
       new_board[pos] = @next_mover_mark
-      #next_mark = get_next_mover(next_mover_mark)
-      next_mark = "X"
-      child = TicTacToeNode.new(new_board, next_mark, @next_mover_mark)
+      next_mark = get_next_mover(next_mover_mark)
+      child = TicTacToeNode.new(new_board, next_mark, pos)
       children << child
     end
     return children
   end
 
   def get_next_mover(curr_mark)
-
+    [:x, :o].reject { |el| el == curr_mark}.first
   end
+
   def all_empty_positions
     result = []
     @board.rows.each_index do |row|
@@ -45,7 +67,7 @@ class TicTacToeNode
 end
 
 if __FILE__ == $PROGRAM_NAME
-  b=Board.new
-  first_node = TicTacToeNode.new(b, "O", "X")
+  b = Board.new
+  first_node = TicTacToeNode.new(b, :o)
   p first_node.children
 end
