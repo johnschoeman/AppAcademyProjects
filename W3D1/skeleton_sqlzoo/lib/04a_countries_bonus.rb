96285c8b766cb317f6a98a8df7381f89fe028dfd
John Schoeman <johnschoeman1617@gmail.com>
2017-08-14 18:25:08 -0700

    Add W3D1 project - SQLzoo

---
+++ W3D1/skeleton_sqlzoo/lib/04a_countries_bonus.rb
@@ -0,0 +1,61 @@
+# == Schema Information
+#
+# Table name: countries
+#
+#  name        :string       not null, primary key
+#  continent   :string
+#  area        :integer
+#  population  :integer
+#  gdp         :integer
+
+require_relative './sqlzoo.rb'
+
+# BONUS QUESTIONS: These problems require knowledge of aggregate
+# functions. Attempt them after completing section 05.
+
+def highest_gdp
+  # Which countries have a GDP greater than every country in Europe? (Give the
+  # name only. Some countries may have NULL gdp values)
+  execute(<<-SQL)
+  SELECT
+    countries.name
+  FROM
+    countries
+  WHERE
+    countries.gdp > (
+      SELECT
+        MAX(gdp)
+      FROM
+        countries
+      WHERE
+        continent = 'Europe'
+    )
+  SQL
+end
+
+def largest_in_continent
+  # Find the largest country (by area) in each continent. Show the continent,
+  # name, and area.
+  execute(<<-SQL)
+  SELECT
+    largest.continent, largest.name, largest.area
+  FROM
+    ( SELECT
+        countries.continent, countries.name, countries.area
+      FROM
+        countries
+      GROUP BY
+        continent
+      HAVING
+        MAX(countries.area)
+    ) AS largest
+  SQL
+end
+
+def large_neighbors
+  # Some countries have populations more than three times that of any of their
+  # neighbors (in the same continent). Give the countries and continents.
+  execute(<<-SQL)
+    
+  SQL
+end
