a18fd017e88cc2f49ddb6df427177b9c9331ef45
John Schoeman <johnschoeman1617@gmail.com>
2017-09-18 16:21:44 -0700

    Create Bench model controller and rails routes

--- W8D1/bench_bnb_app/db/seeds.rb
+++ W8D1/bench_bnb_app/db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+10.times do |i|
+  Bench.create(description: "bench-#{i}", lat: rand(35..39), lng: rand((-122)..(-119)))
+end
